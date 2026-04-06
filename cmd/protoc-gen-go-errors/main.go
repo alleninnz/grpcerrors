d85ee49f4730e50d3f0f0ef12b2fb9926df5cdcd
Xingxing <allenincosmos@gmail.com>
2026-04-06 19:33:04 +1200

    feat(protoc-gen-go-errors): support proto3 optional fields
    Declare FEATURE_PROTO3_OPTIONAL support in the plugin's CodeGeneratorResponse
    so buf no longer emits warnings when processing files with proto3 optional fields.
    
    - Set SupportedFeatures in main.go before returning from the protogen.Run callback

--- cmd/protoc-gen-go-errors/main.go
+++ cmd/protoc-gen-go-errors/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/types/pluginpb"
 )
 
 func main() {
-	protogen.Options{}.Run(generate)
+	protogen.Options{}.Run(func(p *protogen.Plugin) error {
+		p.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
+		return generate(p)
+	})
 }
